633b488c4600b8faedcf2caf4a64cac24a3147d2
XIAOYU ZHOU <xiaoyuz2@andrew.cmu.edu>
2018-10-26 10:20:02 -0400

    Use camera to display sentiment.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cv2.namedWindow("FaceRecognition")
 
 counter = 0
 previousEmotion = "unknow"
-detectionEmotionPer = 1000
+detectionEmotionPer = 2
 while True:
     ret, frame = cam.read()
 
@@ -20,16 +20,18 @@ while True:
     if len(rectangle) > 0:
         top, right, bottom, left = rectangle[0]
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 255))
-    if counter % detectionEmotionPer == 0:
-        img_filename = "faceRecognition.png"
-        cv2.imwrite(img_filename, frame)
-        emotion = sentimentDetection.createSentimentRequest(img_filename)
-        if emotion:
-            previousEmotion = emotion
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, previousEmotion, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        if counter % detectionEmotionPer == 0:
+            img_filename = "faceRecognition.png"
+            cv2.imwrite(img_filename, frame)
+            emotion = sentimentDetection.createSentimentRequest(img_filename)
+
+            if emotion:
+                previousEmotion = emotion
+            font = cv2.FONT_HERSHEY_DUPLEX
+    cv2.putText(frame, previousEmotion, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    counter += 1
     cv2.imshow("FaceRecognition", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(500) & 0xFF == ord('q'):
         break
 
     # time.sleep(0.5)
